1e4bdefb5a4817a6fb5cd0934390e537e33d9cc7
lenz-B <154307607+lenz-B@users.noreply.github.com>
2025-06-21 16:33:36 +0400

    signupform

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,12 @@
+import { SignupForm } from '@/components/signup-form'
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center dark:bg-black py-12 px-4 sm:px-6 lg:px-8">
+      <SignupForm/>
+    </div>
+  )
+}
+
+export default Register
